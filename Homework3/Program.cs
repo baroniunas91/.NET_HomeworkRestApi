8e87110d98c6b58079d2b48d9eea13728f3ae519
baroniunas91 <61449435+baroniunas91@users.noreply.github.com>
2020-12-20 21:16:29 +0200

    Add project files.

---
+++ Homework3/Program.cs
@@ -0,0 +1,35 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync("https://www.cvbankas.lt/?miestas=Vilnius&padalinys%5B%5D=76&keyw=");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            //var responseObject = JsonConvert.DeserializeObject<Post>(responseBody);
+
+            var htmlDoc = new HtmlDocument();
+
+            htmlDoc.LoadHtml(responseBody);
+
+            var links = htmlDoc.DocumentNode.Descendants("h3")
+                .Where(node => node.GetAttributeValue("class", "").Contains("list_h3")).ToList();
+
+            var children = links.Select(l => l.InnerText);
+
+        }
+    }
+}
---
+++ RestApi/Program.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RestApi
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Gaunam user id
+
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            var responseObjectsList = JsonConvert.DeserializeObject<List<User>>(responseBody);
+
+            var user = responseObjectsList.Where(item => item.Name == "Mrs. Dennis Schulist").ToList();
+
+            int userId = user[0].Id;
+
+
+            //Gaunam visus user albumus
+
+            var response2 = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/albums");
+
+            response2.EnsureSuccessStatusCode();
+
+            var responseBody2 = await response2.Content.ReadAsStringAsync();
+
+            var responseObjectsList2 = JsonConvert.DeserializeObject<List<Album>>(responseBody2);
+
+            var albums = responseObjectsList2.Where(item => item.UserId == userId).ToList();
+
+
+            //Gaunam user visu albumu nuotraukas, vienas albumas turi daug nuotraukų
+
+            var response3 = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/photos");
+
+            response3.EnsureSuccessStatusCode();
+
+            var responseBody3 = await response3.Content.ReadAsStringAsync();
+
+            var responseObjectsList3 = JsonConvert.DeserializeObject<List<Photo>>(responseBody3);
+
+            List<string> photos = new List<string>();
+
+            foreach (var item in albums)
+            {
+                foreach (var value in responseObjectsList3)
+                {
+                    if (value.AlbumId == item.Id)
+                    {
+                        photos.Add(value.Url);
+                        Console.WriteLine(value.Url);
+                    }
+                }
+            }
+        }
+    }
+}
